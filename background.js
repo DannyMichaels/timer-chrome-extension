382deaaddfa9c1dc34c8591ab10bdc72ec926f20
Daniel <itzdanielmichael@gmail.com>
2022-09-07 14:21:39 -0400

    add notifications

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ chrome.alarms.create({
   periodInMinutes: 1 / 60, // fire every one second (1 60th of a mintute is a second)
 });
 
-chrome.alarms.onAlarm.addListener((alarm) => {
+const onAlarm = (alarm) => {
   chrome.storage.local.get(['timer'], ({ timer = 0 }) => {
     chrome.storage.local.set({
       timer: timer + 1,
@@ -13,4 +13,24 @@ chrome.alarms.onAlarm.addListener((alarm) => {
       text: `${timer + 1}`,
     });
   });
-});
+
+  // make sure that chrome has notifications enabled in Notifications & Actions settings windows 10...
+  // https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration
+  this.registration.showNotification('Chrome Timer Extension', {
+    body: '1 second has passed!',
+    icon: './icon.png',
+  });
+
+  // const options = {
+  //   type: 'basic',
+  //   iconUrl: './icon.png',
+  //   title: 'Chrome Timer Extension',
+  //   message: '1 second has passed!',
+  // };
+
+  // chrome.notifications.create(options);
+};
+
+chrome.alarms.onAlarm.addListener(onAlarm);
+
+// console.log(this); // => ServiceWorkerGlobalScope
